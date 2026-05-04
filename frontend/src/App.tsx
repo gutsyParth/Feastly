920544275aab4f6582eeac6d110443d2afb16063
Parth Yadav <gutsyparthyadav@gmail.com>
2026-05-04 12:28:21 +0530

    feat(ui): implement navbar search with debouncing, URL query sync, and geolocation-based city detection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,14 @@ import { Toaster } from "react-hot-toast";
 import PublicRoute from "./components/publicRoute";
 import ProtectedRoute from "./components/protectedRoute";
 import SelectRole from "./pages/SelectRole";
+import Navbar from "./components/navbar";
+import Account from "./pages/Accout";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route element={<PublicRoute />}>
             <Route path="/login" element={<Login />} />
@@ -17,6 +20,7 @@ const App = () => {
           <Route element={<ProtectedRoute />}>
             <Route path="/" element={<Home />} />
             <Route path="/select-role" element={<SelectRole />} />
+            <Route path="/account" element={<Account />} />
           </Route>
         </Routes>
         <Toaster />
